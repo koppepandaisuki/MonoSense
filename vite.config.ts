d52e5b87a003a421990ad468f2c30b057cc92c71
koppepandaisuki <koppepandaisukidayo@gmail.com>
2026-03-16 00:09:59 +0900

    ページ追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
